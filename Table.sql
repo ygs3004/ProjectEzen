48ae427872b7a15d384b13d453a5821ba00b9319
윤건수 <ygs3004@naver.com>
2022-10-26 17:43:31 +0900

    db sql create

---
+++ Table.sql
@@ -0,0 +1,56 @@
+drop table board;
+drop table reply;
+drop table users;
+
+CREATE TABLE board(
+    boardNo NUMBER(20) primary key ,
+    title VARCHAR2(200) not null,
+    content VARCHAR2(3000) not null,
+    user_name VARCHAR2(50) not null,
+    regDate DATE DEFAULT SYSDATE,
+    updateDate DATE DEFAULT SYSDATE,
+    boardName varchar2(50) not null
+);
+
+CREATE TABLE reply(
+    replayNo number(15) primary key ,
+    boardNo number(15) not null,
+    reply varchar2(3000) not null,
+    replyer varchar2(250) not null
+);
+
+CREATE TABLE USERS
+(
+    USER_ROLE NUMBER NOT NULL
+    , USER_NAME VARCHAR2(10) NOT NULL
+    , USER_ID VARCHAR2(30) NOT NULL primary key
+    , USER_PW NUMBER NOT NULL
+    , USER_PW2 NUMBER NOT NULL
+    , USER_EMAIL VARCHAR2(15) NOT NULL
+    , USER_PHONE VARCHAR2(10) NOT NULL
+    , USER_GENDER VARCHAR2(30) NOT NULL
+    , USER_SCHOOL VARCHAR2(30) NOT NULL
+);
+
+CREATE TABLE MENTOROOM
+(
+    USER_ID VARCHAR2(20)
+    , NUM NUMBER NOT NULL primary key
+    , TITLE VARCHAR2(200) NOT NULL
+    , NAME VARCHAR2(10) NOT NULL
+    , CAREER VARCHAR2(1000)
+    , SCHOOL VARCHAR2(30) NOT NULL
+    , CAPACITY NUMBER NOT NULL
+    , STUDYSTART DATE NOT NULL
+    , STUDYEND DATE NOT NULL
+    , CONTENT VARCHAR2(1000)
+);
+
+ALTER TABLE MENTOROOM ADD CONSTRAINT USERS FOREIGN KET(USER_ID) REFERENCES USERS(USER_ID);
+
+select * from board;
+select * from reply;
+select * from users;
+select  * from MENTOROOM;
+
+
